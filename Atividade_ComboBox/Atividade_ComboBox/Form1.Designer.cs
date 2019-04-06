5d9bac9fe7e3a381bf29e7309f6df158a5c5a784
Marcelot <marcelojtelles@gmail.com>
2019-04-06 20:15:52 -0300

    Add files via upload

---
+++ Atividade_ComboBox/Atividade_ComboBox/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Atividade_ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbDiasSemana = new System.Windows.Forms.ComboBox();
+            this.btnEstado = new System.Windows.Forms.Button();
+            this.btnDiasSemana = new System.Windows.Forms.Button();
+            this.btnProduto = new System.Windows.Forms.Button();
+            this.cmbProduto = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbPreco = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbCidade = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Estado:";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(131, 35);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(121, 21);
+            this.cmbEstado.TabIndex = 1;
+            this.cmbEstado.SelectedIndexChanged += new System.EventHandler(this.cmbEstado_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dia da Semana";
+            // 
+            // cmbDiasSemana
+            // 
+            this.cmbDiasSemana.FormattingEnabled = true;
+            this.cmbDiasSemana.Location = new System.Drawing.Point(131, 164);
+            this.cmbDiasSemana.Name = "cmbDiasSemana";
+            this.cmbDiasSemana.Size = new System.Drawing.Size(121, 21);
+            this.cmbDiasSemana.TabIndex = 3;
+            this.cmbDiasSemana.SelectedIndexChanged += new System.EventHandler(this.cmbDiasSemana_SelectedIndexChanged);
+            // 
+            // btnEstado
+            // 
+            this.btnEstado.Location = new System.Drawing.Point(113, 73);
+            this.btnEstado.Name = "btnEstado";
+            this.btnEstado.Size = new System.Drawing.Size(121, 23);
+            this.btnEstado.TabIndex = 4;
+            this.btnEstado.Text = "Carregar Estados";
+            this.btnEstado.UseVisualStyleBackColor = true;
+            this.btnEstado.Click += new System.EventHandler(this.btnEstado_Click);
+            // 
+            // btnDiasSemana
+            // 
+            this.btnDiasSemana.Location = new System.Drawing.Point(113, 201);
+            this.btnDiasSemana.Name = "btnDiasSemana";
+            this.btnDiasSemana.Size = new System.Drawing.Size(121, 23);
+            this.btnDiasSemana.TabIndex = 5;
+            this.btnDiasSemana.Text = "Carregar Dias";
+            this.btnDiasSemana.UseVisualStyleBackColor = true;
+            this.btnDiasSemana.Click += new System.EventHandler(this.btnDiasSemana_Click);
+            // 
+            // btnProduto
+            // 
+            this.btnProduto.Location = new System.Drawing.Point(117, 276);
+            this.btnProduto.Name = "btnProduto";
+            this.btnProduto.Size = new System.Drawing.Size(111, 23);
+            this.btnProduto.TabIndex = 6;
+            this.btnProduto.Text = "Carregar Produto";
+            this.btnProduto.UseVisualStyleBackColor = true;
+            this.btnProduto.Click += new System.EventHandler(this.btnProduto_Click);
+            // 
+            // cmbProduto
+            // 
+            this.cmbProduto.FormattingEnabled = true;
+            this.cmbProduto.Location = new System.Drawing.Point(65, 241);
+            this.cmbProduto.Name = "cmbProduto";
+            this.cmbProduto.Size = new System.Drawing.Size(98, 21);
+            this.cmbProduto.TabIndex = 7;
+            this.cmbProduto.SelectedIndexChanged += new System.EventHandler(this.cmbProduto_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Produto";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 244);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Preço";
+            // 
+            // cmbPreco
+            // 
+            this.cmbPreco.FormattingEnabled = true;
+            this.cmbPreco.Location = new System.Drawing.Point(222, 241);
+            this.cmbPreco.Name = "cmbPreco";
+            this.cmbPreco.Size = new System.Drawing.Size(81, 21);
+            this.cmbPreco.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(117, 311);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 24);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Ordene";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(83, 121);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Cidade";
+            // 
+            // cmbCidade
+            // 
+            this.cmbCidade.FormattingEnabled = true;
+            this.cmbCidade.Location = new System.Drawing.Point(131, 118);
+            this.cmbCidade.Name = "cmbCidade";
+            this.cmbCidade.Size = new System.Drawing.Size(121, 21);
+            this.cmbCidade.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 405);
+            this.Controls.Add(this.cmbCidade);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbPreco);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cmbProduto);
+            this.Controls.Add(this.btnProduto);
+            this.Controls.Add(this.btnDiasSemana);
+            this.Controls.Add(this.btnEstado);
+            this.Controls.Add(this.cmbDiasSemana);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbEstado);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbDiasSemana;
+        private System.Windows.Forms.Button btnEstado;
+        private System.Windows.Forms.Button btnDiasSemana;
+        private System.Windows.Forms.Button btnProduto;
+        private System.Windows.Forms.ComboBox cmbProduto;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbPreco;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbCidade;
+    }
+}
+
---
+++ Atividade_ComboBox/Atividade_ComboBox/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Atividade_ComboBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cmbDiasSemana_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEstado_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmbEstado.Items.Clear();
+                cmbEstado.Items.Add("Selecione...");
+
+                cmbEstado.SelectedIndex = 0;
+
+                string[] linhas = System.IO.File.ReadAllLines("F:\\Programação II - C#\\Atividade_ComboBox\\estado.txt");
+
+                foreach (string l in linhas)
+                {
+
+                    //Console.WriteLine(l);
+
+                    cmbEstado.Items.Add(l);
+                }
+
+            }
+
+            catch (Exception ee)
+            {
+
+                MessageBox.Show("Falha na abertura do arquivo estado.txt"+ee.Message);
+
+            }
+        }
+
+        private void btnDiasSemana_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                cmbDiasSemana.Items.Add("Selecione...");
+
+                cmbDiasSemana.SelectedIndex = 0;
+
+                string[] linhas = System.IO.File.ReadAllLines("F:\\Programação II - C#\\Atividade_ComboBox\\diassemana.txt");
+
+                foreach (string l in linhas)
+                {
+
+                    //Console.WriteLine(l);
+
+                    cmbDiasSemana.Items.Add(l);
+                }
+
+            }
+
+            catch (Exception ee)
+            {
+
+                MessageBox.Show("Falha na abertura do arquivo diassemana.txt");
+
+            }
+        }
+
+        private void btnProduto_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                cmbProduto.Items.Add("Selecione...");
+
+                cmbProduto.SelectedIndex = 0;
+
+                string[] linhas = System.IO.File.ReadAllLines("F:\\Programação II - C#\\Atividade_ComboBox\\produtos.txt");
+
+                foreach (string l in linhas)
+                {
+
+                    //Console.WriteLine(l);
+
+                    cmbProduto.Items.Add(l);
+                }
+
+                cmbPreco.Items.Add("Selecione...");
+
+                cmbPreco.SelectedIndex = 0;
+
+                string[] linhas2 = System.IO.File.ReadAllLines("F:\\Programação II - C#\\Atividade_ComboBox\\preço.txt");
+
+                foreach (string l2 in linhas2)
+                {
+
+                    //Console.WriteLine(l);
+
+                    cmbPreco.Items.Add(l2);
+                }
+
+            }
+
+            catch (Exception ee)
+            {
+
+                MessageBox.Show("Falha na abertura do arquivo produtos.txt");
+                Console.WriteLine(ee);
+
+            }
+        }
+
+        private void cmbProduto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //MessageBox.Show("F");
+            try
+            {
+                cmbPreco.SelectedIndex = cmbProduto.SelectedIndex;
+            }
+            catch (Exception ee)
+            {
+                //MessageBox.Show("Fooooo");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmbPreco.Items.Clear();
+                cmbProduto.Items.Clear();
+
+                cmbProduto.Items.Add("Selecione...");
+
+                cmbProduto.SelectedIndex = 0;
+
+                string[] linhas = System.IO.File.ReadAllLines("F:\\Programação II - C#\\Atividade_ComboBox\\produtos.txt");
+
+                
+
+                cmbPreco.Items.Add("Selecione...");
+
+                cmbPreco.SelectedIndex = 0;
+
+                string[] linhas2 = System.IO.File.ReadAllLines("F:\\Programação II - C#\\Atividade_ComboBox\\preço.txt");
+
+                foreach (string c in linhas2)
+                {
+                    Console.WriteLine(c);
+                }
+
+                Array.Sort(linhas2,linhas);
+                foreach (string c in linhas2)
+                {
+                    Console.WriteLine(c);
+                }
+                
+                foreach (string l2 in linhas2)
+                {
+
+                    //Console.WriteLine(l);
+
+                    cmbPreco.Items.Add(l2);
+                }
+
+                foreach (string l in linhas)
+                {
+
+                    //Console.WriteLine(l);
+
+                    cmbProduto.Items.Add(l);
+                }
+
+            }
+
+            catch (Exception ee)
+            {
+
+                MessageBox.Show("Falha na abertura do arquivo produtos.txt");
+                Console.WriteLine(ee);
+
+            }
+        }
+
+        private void cmbEstado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                //cmbCidade.SelectedIndex = cmbEstado.SelectedIndex;
+                if (cmbEstado.SelectedIndex != 0)
+                {
+                    //MessageBox.Show("selecionou"+cmbEstado.SelectedItem.ToString());
+                    cmbCidade.Items.Clear();
+                    cmbCidade.Items.Add("Selecione...");
+
+                    cmbCidade.SelectedIndex = 0;
+
+                    string[] linhas2 = System.IO.File.ReadAllLines("F:\\Programação II - C#\\Atividade_ComboBox\\cidade.txt");
+                    int linhaLida = 0;
+                    foreach (string l2 in linhas2)
+                    {
+                        if(l2.Contains(cmbEstado.SelectedItem.ToString())){
+                            //MessageBox.Show("ccccccc" + linhaLida);
+                            linhaLida++;
+                            cmbCidade.Items.Add(l2);
+                        }
+                        //Console.WriteLine(l2);
+                    }
+
+                }
+            
+
+            
+                
+                
+
+            }
+            catch (Exception ee)
+            {
+                //MessageBox.Show("Erro.");
+            }
+        }
+    }
+}
